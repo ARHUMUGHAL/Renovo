0ab1a421e4a9834eeae5d67a3f535ccb63041d7d
Arhum Mughal <arhummughal@gmail.com>
2025-01-12 23:39:47 +0500

    Navbar and Herobox Responsive Changes

--- src/components/Herobox.js
+++ src/components/Herobox.js
@@ -52,8 +52,6 @@ const Herobox = () => {
 
   },[])
 
-
-
   useEffect(()=>{
 
     gsap.fromTo(
@@ -95,13 +93,13 @@ const Herobox = () => {
 
   return (
     <div ref={heroboxRef} className="herobox w-screen h-[110vh] relative bg-[#202020]">
-      <div className="herobox-image absolute right-[-5%] w-[85%] h-full">
+      <div className="herobox-image absolute right-[-5%] w-[85%] h-full lg:w-[80%]">
         <img src={Image1} className="w-full h-full object-cover" />
       </div>
-      <div className="blur-box w-[70vw] h-[25%] flex items-center justify-center absolute top-[70%] right-[90px]">
+      <div className="blur-box w-[88vw] h-[35%] flex items-center justify-center absolute top-[60%] right-[40px] md:w-[92vw] lg:w-[68vw] lg:h-[25%] lg:top-[70%] xl:h-[20%]">
         <div className=" bg-[#323232] w-full h-full backdrop-blur-[17px] absolute opacity-[60%]"></div>
-        <div className="w-full px-10 h-full flex items-center absolute justify-between font-popins">
-          <div className="w-[300px] text-white">
+        <div className=" w-full p-10 h-full flex flex-col items-center absolute justify-between font-popins lg:flex-row lg:p-5 lg:items-start lg:overflow-hidden">
+          <div className="w-full text-white text-[12px] sm:text-[13px] md:text-[15px] lg:w-[30%] lg:text-[14px]">
             <p>
               We specialize in comprehensive home renovations, from design to
               construction, delivering quality materials and expert
@@ -109,23 +107,23 @@ const Herobox = () => {
             </p>
           </div>
 
-          <div className="w-[300px] h-[80px] font-popins flex flex-col justify-between text-white">
+          <div className="w-full h-[50px] font-popins flex flex-col justify-between text-white lg:w-[30%]">
             <h4 className="font-semibold">Craftsmanship</h4>
-            <p>Unmatched attention to detail in every renovation project</p>
+            <p className="text-[12px] w-[80%] sm:text-[13px] md:text-[15px] lg:text-[14px]">Unmatched attention to detail in every renovation project</p>
           </div>
-          <div className="w-[300px] h-[80px] font-popins flex flex-col justify-between text-white">
+          <div className="w-full h-[50px] font-popins flex flex-col justify-between text-white lg:w-[30%]">
             <h4 className="font-semibold">Durability</h4>
-            <p>High-Quality materials designed to last through years of wear</p>
+            <p className="text-[12px] w-[80%] sm:text-[13px] sm:w-[100%] md:text-[15px] lg:text-[14px]">High-Quality materials designed to last through years of wear</p>
           </div>
         </div>
       </div>
-      <div className="w-[40%] h-full absolute">
-        <h2 className="h2 text-white font-playfair text-[40px] absolute top-[28%] left-[50px]">Home</h2>
-        <h1 className="h1-low-opacity text-white font-playfair text-[128px] absolute top-[30%] left-[20px] opacity-10">Renovation</h1>
-        <h1 className="h1-high-opacity text-white font-playfair text-[128px] absolute top-[35%] left-[50px]">Renovation</h1>
-        <p className="bio text-white font-popins w-[230px] text-[15px] absolute top-[65%] left-[50px]">Transforming your space with expert renovation services that add value and style</p>
-        <a className="white-btn inline-block w-[210px] h-[36px] bg-white text-black absolute top-[80%] flex items-center justify-center font-popins rounded-[5px] font-semibold left-[50px]" href="/">Get a Free Quote</a>
-        <div className="w-[150px] h-[50px] flex items-center justify-between absolute top-[92%] left-[50px] text-white">
+      <div className="w-[70%] h-full absolute">
+        <h2 className="h2 text-white font-playfair text-[25px] absolute top-[18%] sm:top-[16%] left-[50px] lg:text-[30px] xl:text-[35px] xl:top-[25%]">Home</h2>
+        <h1 className="h1-low-opacity text-white font-playfair text-[80px] absolute top-[20%] left-[20px] opacity-10 sm:text-[95px] sm:top-[18%] lg:text-[120px] lg:top-[20%] xl:top-[30%] xl:text-[128px]">Renovation</h1>
+        <h1 className="h1-high-opacity text-white font-playfair text-[80px] absolute top-[25%] left-[50px] sm:text-[95px] sm:top-[22%] lg:text-[120px] lg:top-[25%] xl:top-[35%] xl:text-[128px]">Renovation</h1>
+        <p className="bio text-white font-popins w-[100%] text-[15px] absolute top-[42%] sm:top-[40%] md:w-[80%] left-[50px] lg:text-[18px] lg:w-[50%] lg:top-[50%] xl:top-[65%] xl:text-[14px] xl:w-[30%]">Transforming your space with expert renovation services that add value and style</p>
+        <a className="white-btn inline-block w-[150px] h-[36px] bg-white text-[12px] text-black absolute top-[50%] flex items-center justify-center font-popins rounded-[5px] font-semibold left-[50px] sm:w-[180px] sm:text-[15px] md:w-[200px] md:text-[16px] lg:top-[70%] lg:w-[180px] xl:top-[80%]" href="/">Get a Free Quote</a>
+        <div className="w-[180px] hidden h-[50px] items-center justify-between absolute top-[92%] left-[50px] text-white lg:flex">
         <CiFacebook className="text-[30px] "/>
         <CiInstagram className="text-[30px] " />
         <CiLinkedin className="text-[30px] " />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,71 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React from 'react'
+import React, { useRef } from 'react'
+import { MdOutlineSegment } from "react-icons/md";
+
 
 
 const Navbar = () => {
 
-  useGSAP(()=>{
-    gsap.fromTo('.navbar',{y : '-100%' , opacity : 0},{y : 0 , opacity : 1, duration : 2})
+  useGSAP(() => {
+    gsap.fromTo('.navbar', { y: '-100%', opacity: 0 }, { y: 0, opacity: 1, duration: 2 })
   })
 
+  const toggleMenuRef = useRef(null)
+
+
+  function showToggleMenu() {
+    
+
+    const toggleMenu = toggleMenuRef.current
+
+    if(toggleMenu){
+      toggleMenu.style.display = 'inline-block'
+      gsap.fromTo(toggleMenu,{x : '120%',opacity : 0},{x : '99%',opacity : 1,duration : 1})
+
+    }
+  }
+
+  function hideToggleMenu(){
+
+    const toggleMenu = toggleMenuRef.current
+
+    if(toggleMenu){
+      toggleMenu.style.display = 'hidden'
+      gsap.to(toggleMenu,{x : '120%',opacity : 0,duration : 1})
+
+    }
+  }
+
+
   return (
-    <div className='navbar absolute z-[999] w-full px-[50px] flex justify-between items-center text-[36px] h-[100px] text-white'>
-      <div className="nav-logo font-playfair">Renovo</div>
-      <div className="nav-links w-[45%] font-popins">
-        <ul className='flex text-[18px] justify-between items-center' >
-          <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">About</a></li>
-          <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Projects</a></li>
-          <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Services</a></li>
-          <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Blog</a></li>
-          <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Contact</a></li>
-          <li><a href="/" className='hover:bg-transparent hover:border-[1px] hover:text-white transition duration-[0.60s`] w-[115px] h-[36px] bg-white text-black inline-block flex items-center justify-center rounded-[5px]'>Login</a></li>
-        </ul>
+    <div className='navbar fixed top-0 left-0 z-[999] w-full px-[20px]  flex justify-between items-start text-[36px] h-screen text-white'>
+      <div className='w-full h-[100px] z-[999] flex justify-between items-center'>
+        <div className="nav-logo font-playfair">Renovo</div>
+        <div className='cursor-pointer md:hidden' onClick={showToggleMenu}><MdOutlineSegment /></div>
+        <div className="hidden nav-links md:inline-block w-[45%] md:w-[70%] lg:w-[65%] xl:w-[50%] 2xl:w-[45%] font-popins">
+          <ul className='flex text-[18px] justify-between items-center' >
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">About</a></li>
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Projects</a></li>
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Services</a></li>
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Blog</a></li>
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Contact</a></li>
+            <li><a href="/" className='hover:bg-transparent hover:border-[1px] hover:text-white transition duration-[0.60s`] w-[115px] h-[36px] bg-white text-black inline-block flex items-center justify-center rounded-[5px]'>Login</a></li>
+          </ul>
+        </div>
+      </div>
+      <div ref={toggleMenuRef} className='toggle-menu pr-[40px] hidden absolute w-[50%] h-full bg-[#262626]'>
+        <div className='w-100% h-[50%] relative top-[15%]'>
+        <ul className='flex flex-col text-[18px] h-full items-end justify-between' >
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">About</a></li>
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Projects</a></li>
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Services</a></li>
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Blog</a></li>
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Contact</a></li>
+            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s] text-red-900 cursor-pointer' onClick={hideToggleMenu}><p className='pointer-events-none'>Close</p></li>
+            <li><a href="/" className='hover:bg-transparent hover:border-[1px] hover:text-white transition duration-[0.60s`] w-[115px] h-[36px] bg-white text-black inline-block flex items-center justify-center rounded-[5px]'>Login</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   )
