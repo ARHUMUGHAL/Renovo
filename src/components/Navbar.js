bf3d8546a3c0a09b3708176606c2f2ea5240c923
Arhum Mughal <arhummughal@gmail.com>
2025-01-16 01:05:14 +0500

    Update About JS File

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { MdOutlineSegment } from "react-icons/md";
+
 
 
 
@@ -12,37 +13,64 @@ const Navbar = () => {
   })
 
   const toggleMenuRef = useRef(null)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const ToggleMenu = () => {
+      const toggleMenu = toggleMenuRef.current;
+      if (toggleMenu) {
+        if (isMenuOpen) {
+          gsap.to(toggleMenu, { x: '120%', opacity: 0, duration: 1, onComplete: () => toggleMenu.style.display = 'none' });
+        }
+        else {
+          toggleMenu.style.display = 'inline-block';
+          gsap.fromTo(toggleMenu, { x: '120%', opacity: 0 }, { x: '100%', opacity: 1, duration: 1 });
+        }
+        setIsMenuOpen(!isMenuOpen);
+      }
 
-  function showToggleMenu() {
-    
-
-    const toggleMenu = toggleMenuRef.current
-
-    if(toggleMenu){
-      toggleMenu.style.display = 'inline-block'
-      gsap.fromTo(toggleMenu,{x : '120%',opacity : 0},{x : '99%',opacity : 1,duration : 1})
-
-    }
   }
 
-  function hideToggleMenu(){
 
-    const toggleMenu = toggleMenuRef.current
+  // function showToggleMenu() {
 
-    if(toggleMenu){
-      toggleMenu.style.display = 'hidden'
-      gsap.to(toggleMenu,{x : '120%',opacity : 0,duration : 1})
 
+  //   const toggleMenu = toggleMenuRef.current
+
+  //   if(toggleMenu){
+  //     toggleMenu.style.display = 'inline-block'
+  //     gsap.fromTo(toggleMenu,{x : '120%',opacity : 0},{x : '99%',opacity : 1,duration : 1})
+
+  //   }
+  // }
+
+  // function hideToggleMenu(){
+
+  //   const toggleMenu = toggleMenuRef.current
+
+  //   if(toggleMenu){
+  //     toggleMenu.style.display = 'hidden'
+  //     gsap.to(toggleMenu,{x : '120%',opacity : 0,duration : 1})
+
+  //   }
+  // }
+
+  const [navbar, setNavbar] = useState(false)
+
+  const changeBackground = () => {
+    if (window.scrollY >= 2) {
+      setNavbar(true)
+    }
+    else {
+      setNavbar(false)
     }
   }
-
+  window.addEventListener('scroll', changeBackground)
 
   return (
     <div className='navbar fixed top-0 left-0 z-[999] w-full px-[20px]  flex justify-between items-start text-[36px] h-screen text-white'>
-      <div className='w-full h-[100px] z-[999] flex justify-between items-center'>
+      <div className={navbar ? 'w-full h-[100px] z-[999] flex justify-between items-center bg-black-500 backdrop-blur-[8px] transition duration-[.5s] linear' : 'w-full h-[100px] z-[999] flex justify-between items-center transition duration-[.5s] linear'}>
         <div className="nav-logo font-playfair">Renovo</div>
-        <div className='cursor-pointer md:hidden' onClick={showToggleMenu}><MdOutlineSegment /></div>
+        <div className='cursor-pointer md:hidden' onClick={ToggleMenu}><MdOutlineSegment /></div>
         <div className="hidden nav-links md:inline-block w-[45%] md:w-[70%] lg:w-[65%] xl:w-[50%] 2xl:w-[45%] font-popins">
           <ul className='flex text-[18px] justify-between items-center' >
             <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">About</a></li>
@@ -56,13 +84,13 @@ const Navbar = () => {
       </div>
       <div ref={toggleMenuRef} className='toggle-menu pr-[40px] hidden absolute w-[50%] h-full bg-[#262626]'>
         <div className='w-100% h-[50%] relative top-[15%]'>
-        <ul className='flex flex-col text-[18px] h-full items-end justify-between' >
+          <ul className='flex flex-col text-[18px] h-full items-end justify-between' >
             <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">About</a></li>
             <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Projects</a></li>
             <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Services</a></li>
             <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Blog</a></li>
             <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s]'><a href="/">Contact</a></li>
-            <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s] text-red-900 cursor-pointer' onClick={hideToggleMenu}><p className='pointer-events-none'>Close</p></li>
+            {/* <li className='hover:text-[#aaaaaa] transition-colors duration-[0.60s] text-red-900 cursor-pointer' onClick={hideToggleMenu}><p className='pointer-events-none'>Close</p></li> */}
             <li><a href="/" className='hover:bg-transparent hover:border-[1px] hover:text-white transition duration-[0.60s`] w-[115px] h-[36px] bg-white text-black inline-block flex items-center justify-center rounded-[5px]'>Login</a></li>
           </ul>
         </div>
